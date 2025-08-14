939ffeeb9340fe00c683e84494c297403adfffd2
JahongLiu <jahongliu@gmail.com>
2025-08-14 11:51:44 -0400

    Grand lambda access to dynamodb tables and ssm secret

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -21,4 +21,64 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+data "aws_iam_policy_document" "lambda_dynamodb" {
+  statement {
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:Query",
+      "dynamodb:UpdateItem",
+      "dynamodb:Scan"
+    ]
+    resources = [
+      var.dynamodb_table_arn,
+      "${var.dynamodb_table_arn}/index/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_dynamodb_policy" {
+  name   = "${var.project_name}-lambda-dynamodb-${var.environment}"
+  policy = data.aws_iam_policy_document.lambda_dynamodb.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attach" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+}
+
+data "aws_iam_policy_document" "lambda_users_table" {
+  statement {
+    actions   = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:Query", "dynamodb:UpdateItem"]
+    resources = [var.users_table_arn]
+  }
+}
+
+resource "aws_iam_policy" "lambda_users_policy" {
+  name   = "${var.project_name}-lambda-users-${var.environment}"
+  policy = data.aws_iam_policy_document.lambda_users_table.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_users_attach" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_users_policy.arn
+}
+
+data "aws_iam_policy_document" "lambda_ssm" {
+  statement {
+    actions   = ["ssm:GetParameter"]
+    resources = [var.jwt_secret_arn]
+  }
+}
+
+resource "aws_iam_policy" "lambda_ssm_policy" {
+  name   = "${var.project_name}-lambda-ssm-${var.environment}"
+  policy = data.aws_iam_policy_document.lambda_ssm.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ssm_attach" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_ssm_policy.arn
+}
+
 
--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -1,4 +1,7 @@
 variable "project_name" { type = string }
 variable "environment"  { type = string }
+variable "dynamodb_table_arn" { type = string }
+variable "users_table_arn"    { type = string }
+variable "jwt_secret_arn"     { type = string }
 
 
