38f3c940b3776f16c37f2ed28882e868dbe53af9
JahongLiu <jahongliu@gmail.com>
2025-08-14 15:15:17 -0400

    Fix

--- terraform/modules/api/main.tf
+++ terraform/modules/api/main.tf
@@ -35,25 +35,18 @@ resource "aws_lambda_permission" "apigw_invoke_auth" {
 resource "aws_api_gateway_deployment" "auth_deployment" {
   depends_on = [aws_api_gateway_integration.login_lambda]
   rest_api_id = aws_api_gateway_rest_api.auth_api.id
-  stage_name  = var.environment
 }
 
-resource "aws_api_gateway_method_response" "login_200" {
-  rest_api_id = aws_api_gateway_rest_api.auth_api.id
-  resource_id = aws_api_gateway_resource.login.id
-  http_method = aws_api_gateway_method.login_post.http_method
-  status_code = "200"
+resource "aws_api_gateway_stage" "auth_stage" {
+  rest_api_id   = aws_api_gateway_rest_api.auth_api.id
+  deployment_id = aws_api_gateway_deployment.auth_deployment.id
+  stage_name    = var.environment
 }
 
-resource "aws_api_gateway_integration_response" "login_integration_200" {
-  rest_api_id = aws_api_gateway_rest_api.auth_api.id
-  resource_id = aws_api_gateway_resource.login.id
-  http_method = aws_api_gateway_method.login_post.http_method
-  status_code = aws_api_gateway_method_response.login_200.status_code
-}
+// Remove explicit method/integration responses for proxy integration
 
 output "api_url" {
-  value = aws_api_gateway_deployment.auth_deployment.invoke_url
+  value = aws_api_gateway_stage.auth_stage.invoke_url
 }
 
 # Region data source for building Lambda APIGW URI
